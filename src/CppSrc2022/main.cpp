9680fd4a933bb3398e847308c2ab5628b7890748
OrhanOZTURK <orhn.ozturk@gmail.com>
2023-01-01 12:28:38 +0300

    Referanslar ve pointer karsılastırmaları incelendi

--- src/CppSrc2022/main.cpp
+++ src/CppSrc2022/main.cpp
@@ -2,26 +2,15 @@
 
 using namespace std;
 
+
+
 int main()
 {
-    //bir pointer referans olabilirmi
-
-    int x = 10;
-    int y = 30;
-
-    int *p{ &x };
-    int * &rp = p; //int * tur belirtir ve pointe p turu saklanır rp de
 
 
-    rp = &y; // p ye y'ni adresi atandı
-
-    *rp = 99; //  y = 99
-
-    cout << "y : " << y << "\n";
-
-    return 0;
 }
 
 
 /***************************************************/
 /***************************************************/
+
