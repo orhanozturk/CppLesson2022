19a6ccadeb70f5dab6697bb386a97d5af37b6caf
OrhanOZTURK <orhn.ozturk@gmail.com>
2023-01-06 22:27:01 +0300

    auto, decltype , default function arguments ve Rvalue referance konuları işlendi

--- src/CppSrc2022/main.cpp
+++ src/CppSrc2022/main.cpp
@@ -3,14 +3,24 @@
 using namespace std;
 
 
+struct Data{
+    int mx;
+};
 
 int main()
 {
-
+    int x = 10;
+    int &r = x;
+    decltype (r) y = x; //sentaks hatası ilk deger verilmediğinden
+    //int & turudur, auto daki gibi referanslık düşmez
 
 }
 
 
+
 /***************************************************/
 /***************************************************/
+
+
+
 
