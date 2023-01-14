dbbf83c93feef49bc63fe20d504c48a75752153b
OrhanOZTURK <orhn.ozturk@gmail.com>
2023-01-14 13:02:23 +0300

    constexpt devam, type cast ve function overloaing

--- src/CppSrc2022/main.cpp
+++ src/CppSrc2022/main.cpp
@@ -1,55 +1,19 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
+//function overloading
 
-//sonuc ürettiği zaman compile time da
-//onliner bir fonksiyon
-constexpr int square(int a)
-{
-    return a * a;
-}
-
-constexpr int ndigit(int ival)
-{
-    if(ival == 0)
-        return 1;
-
-    int digit_count = 0;
-
-    while (ival) {
-        ++digit_count;
-        ival /= 10;
-    }
-
-    return digit_count;
-}
-
+//void fuu(int); //fuu, int
+//void fuu(bool); //fuu, bool
 
 int main()
 {
-    constexpr int a = 10;
-    constexpr int b = 20;
-
-    square(a); //sabir ifade
-
-    int arr[square(a)];
-
-    square(a * b + 3);
-
-    int dArr[square(a * b + 3)];
-
-
-    ndigit(a * b);
-    ndigit(a * b + 1200);
-
-
-    ndigit(square(a * b + 3));
-
-    int ar[ndigit(square(a * b + 3))] = { 0 };
 
 
 }
+
 
 /***************************************************/
 /***************************************************/
