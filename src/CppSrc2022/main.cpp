c43edcf13873349e553b44cab5db0e60998fea8d
OrhanOZTURK <orhn.ozturk@gmail.com>
2023-03-05 11:46:28 +0300

    copy ctor ve copy assingment operator

--- src/CppSrc2022/main.cpp
+++ src/CppSrc2022/main.cpp
@@ -2,27 +2,102 @@
 
 using namespace std;
 
-
 //.hpp
-
-class Member{
+class MyString{
 public:
-    Member(int);
-};
+    MyString(const char *pstr) : mlen{strlen(pstr)}
+    {
+        mp = static_cast<char *>(malloc(mlen + 1));
 
-class Myclass{
+        if(!mp){
+            cerr << "bellek yetersiz\n";
+            exit(EXIT_FAILURE);
+        }
+
+        cout << "consructor this : " << this << "\n";
+        cout << "the address of allocated block : " << (void *)mp << "\n";
+        strcpy(mp, pstr);
+    }
+
+    ~MyString(){
+        cout << "dtor this : " << this << "\n";
+
+        if(mp){
+            free(mp);
+        }
+
+        cout << "the address of allocated block : " << (void *)mp << "\n";
+    }
+
+    //user tarafından copy ctor
+    //deep copy
+    MyString(const MyString &other) : mlen{other.mlen}
+    {
+        cout << "Mystring copy ctor this : " << this << "\n";
+
+        mp = static_cast<char *>(malloc(mlen + 1));
+        if(!mp){
+            cerr << "bellek yetersiz\n";
+            exit(EXIT_FAILURE);
+        }
+        cout << "consructor this : " << this << "\n";
+        cout << "the address of allocated block : " << (void *)mp << "\n";
+        strcpy(mp, other.mp);
+
+    }
+
+    size_t length()const{
+        return mlen;
+    }
+
+    void print()const{
+        cout << "[" << mp << "]\n";
+    }
+
+    //user tarafından copy assigment func
+    //deep copy
+    MyString &operator=(const MyString &other)
+    {
+        if(this == &other)
+            return *this;
+
+        free(mp);
+        mlen = other.mlen;
+
+        mp = static_cast<char *>(malloc(mlen + 1));
+        if(!mp){
+            cerr << "bellek yetersiz\n";
+            exit(EXIT_FAILURE);
+        }
+
+        cout << "consructor this : " << this << "\n";
+        cout << "the address of allocated block : " << (void *)mp << "\n";
+        strcpy(mp, other.mp);
+
+        return *this;
+    }
+
 private:
-    Member mx;
-
-public:
-
+    size_t mlen;
+    char *mp;
 };
 
-//.cpp
+void func(MyString str)
+{
+    cout << "func cagrildi\n";
+    str.print();
+    cout << "func sona eriyor\n";
+}
 
 int main()
 { 
-    Myclass m; //imlicity deleted
+    MyString s1{"Orhan OZTURK"};
+    s1.print();
+
+    s1 = s1; //self-assigment
+
+    s1.print();
+
 }
 
 /***************************************************/
